4a02c3bc6d79d4ea8155075042b130ae271f61cb
CrushedByAMountainOfBooks <ymyputt@gmail.com>
2026-06-06 21:11:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseStaticFiles();
+app.MapGet("/", () => Results.Redirect("/index.html"));
+app.Run();
